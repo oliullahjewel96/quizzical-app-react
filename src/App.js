376339d8a5228242a89c1ae6f7ce11a96fd666e7
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-05-19 00:46:55 +0600

    Added conditional rendering for separate screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
+import Quiz from "./components/Quiz";
 function App() {
+  const [showScreen, setShowScreen] = useState(false);
+
+  function openSeparateScreen() {
+    setShowScreen((prevShowScreen) => !prevShowScreen);
+  }
   return (
     <div className="App">
-      <div className="container">
-        <h1 className="heading">Quizzical</h1>
-        <p className="text">To start a quiz please click on the button</p>
-        <button className="btn">Start Quiz</button>
-      </div>
+      {showScreen ? (
+        <Quiz />
+      ) : (
+        <div className="container">
+          <h1 className="heading">Quizzical</h1>
+          <p className="text">To start a quiz please click on the button</p>
+          <button className="btn" onClick={openSeparateScreen}>
+            Start Quiz
+          </button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/Quiz.js
@@ -0,0 +1,9 @@
+const Quiz = () => {
+  return (
+    <div className="quiz">
+      <h1>Quiz</h1>
+    </div>
+  );
+};
+
+export default Quiz;
