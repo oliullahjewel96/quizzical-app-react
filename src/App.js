39bec26d2647789a850203e69b2341188b3a89df
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-05-19 00:01:23 +0600

    Implemented UI for the quizz app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="heading">Quizzical</h1>
+        <p className="text">To start a quiz please click on the button</p>
+        <button className="btn">Start Quiz</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
