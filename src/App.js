09604cab29e12afff6c0e40f7a22549de2f4ebd1
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-05-21 01:13:52 +0600

    Shifted all logic from Quiz to App Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Quiz from "./components/Quiz";
+import { nanoid } from "nanoid";
 function App() {
   const [showScreen, setShowScreen] = useState(false);
+  const [QuizData, setQuizData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&category=21&type=multiple")
+      .then((res) => res.json())
+      .then((data) =>
+        setQuizData(
+          data.results.map((question) => {
+            return {
+              ...question,
+              id: nanoid(),
+              answers: [...question.incorrect_answers, question.correct_answer],
+              isSelected: false,
+            };
+          })
+        )
+      );
+  }, []);
+
+  function handleSelect(id, isSelected) {
+    setQuizData((prevQuestions) =>
+      prevQuestions.map((question) => {
+        console.log(question);
+        return question.id === id
+          ? { ...question, isSelected: !isSelected }
+          : question;
+      })
+    );
+  }
+
+  const newQuizData = QuizData.map((question) => {
+    return (
+      <Quiz
+        key={question.id}
+        id={question.id}
+        question={question.question}
+        answers={question.answers}
+        isSelected={question.isSelected}
+        handleSelect={handleSelect}
+      />
+    );
+  });
 
   function openSeparateScreen() {
     setShowScreen((prevShowScreen) => !prevShowScreen);
@@ -9,7 +52,21 @@ function App() {
   return (
     <div className="App">
       {showScreen ? (
-        <Quiz />
+        <div className="quiz-container">
+          {newQuizData}
+
+          <button
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "auto",
+            }}
+            className="btn"
+          >
+            Check answers
+          </button>
+        </div>
       ) : (
         <div className="container">
           <h1 className="heading">Quizzical</h1>
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,99 +1,31 @@
-import { useState, useEffect } from "react";
-
-const Quiz = () => {
-  const [questions, setQuestions] = useState([]);
-  useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=5&category=21&type=multiple")
-      .then((res) => res.json())
-      .then((data) => setQuestions(data.results));
-  }, []);
-  console.log(questions);
-  const newQuestion = questions.map((question) => {
-    return (
-      <>
-        <div className="questions" key={question.id}>
-          <h3>{question.question}</h3>
-          <div className="options">
-            <h6>{question.correct_answer}</h6>
-            {question.incorrect_answers.map((option) => {
-              return <h6>{option}</h6>;
-            })}
-          </div>
-        </div>
-        <hr />
-      </>
-    );
-  });
+import React from "react";
+import { nanoid } from "nanoid";
+const Quiz = (props) => {
+  const styles = {
+    backgroundColor: props.isSelected ? "#d6dbf5" : "transparent",
+  };
   return (
-    <div className="quiz-container">
-      {newQuestion}
-
-      {/* <div className="questions">
-        {newQuestion}
+    <>
+      <div className="questions">
+        <h3>{props.question}</h3>
         <div className="options">
-          <h6 className="isSelected">Liverpool </h6>
-          <h6>Manchester United </h6>
-          <h6>Chelsea </h6>
-          <h6>Arsenal </h6>
+          {props.answers.map((answer) => {
+            const obj = { ...answer, id: nanoid() };
+            return (
+              <h6
+                key={obj.id}
+                className="correct"
+                style={styles}
+                onClick={() => props.handleSelect(obj.id)}
+              >
+                {answer}{" "}
+              </h6>
+            );
+          })}
         </div>
       </div>
       <hr />
-      <div className="questions">
-        <h3>
-          Which one is the most successful Football club and also the best club
-          in England. can you guess?Which one is the most successful Football
-          club and also the best club in England. can you guess?
-        </h3>
-        <div className="options">
-          <h6 className="isSelected">Liverpool </h6>
-          <h6>Manchester United </h6>
-          <h6>Chelsea </h6>
-          <h6>Arsenal </h6>
-        </div>
-      </div>
-      <hr />
-      <div className="questions">
-        <h3>Which one is the most successful Football club in England?</h3>
-        <div className="options">
-          <h6>Liverpool </h6>
-          <h6>Manchester United </h6>
-          <h6>Chelsea </h6>
-          <h6>Arsenal </h6>
-        </div>
-      </div>
-      <hr />
-      <div className="questions">
-        <h3>Which one is the most successful Football club in England?</h3>
-        <div className="options">
-          <h6>Liverpool </h6>
-          <h6 className="wrong">Manchester United </h6>
-          <h6>Chelsea </h6>
-          <h6>Arsenal </h6>
-        </div>
-      </div>
-      <hr />
-      <div className="questions">
-        <h3>Which one is the most successful Football club in England?</h3>
-        <div className="options">
-          <h6 className="correct">Liverpool </h6>
-          <h6>Manchester United </h6>
-          <h6>Chelsea </h6>
-          <h6>Arsenal </h6>
-        </div>
-      </div> */}
-      {/* <hr style={{ marginBottom: "10px" }} /> */}
-      <button
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          margin: "auto",
-        }}
-        className="btn"
-      >
-        Check answers
-      </button>
-    </div>
+    </>
   );
 };
 
